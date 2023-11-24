292a2e8d970a70bbc5be3db134031cd863d29f93
burzumba <belyakov38@gmail.com>
2023-11-24 17:31:43 +0100

    добавил метод удаления проекта, сделал метод удаления всех проектов по коду

--- removeallprojects.py
+++ removeallprojects.py
@@ -1,14 +1,19 @@
 import methods.getallprojects as get
+import methods.removeprojectbycode as remove
 import json
 
 
-def remove_all_projects():
-    data = json.loads(get.get_all_projects("token").text)
+def remove_all_projects(token):
+    data = json.loads(get.get_all_projects(token).text)
     amount_of_projects = data['result']['total']
     project_list = []
     for i in range(0, amount_of_projects):
         project_list.append(data['result']['entities'][i]['code'])
-    return project_list
 
-
-print(remove_all_projects())
+    for project_code in range(0, len(project_list)):
+        remove_response = remove.remove_project_by_code(token, project_code)
+        remove_status = json.loads(remove_response.text)['status']
+        print(remove_status)
+        print(remove_response.text)
+        if remove_status is False:
+            break
