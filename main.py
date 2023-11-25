d429747dc87db1212ef87c868945a960b98ae961
burzumba <belyakov38@gmail.com>
2023-11-25 12:22:23 +0100

    переименовал файлы в snake_case, сгруппировал методы для вложений и проектов

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import clearall
+from steps import clear_all
 import tokens
 import threading
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    t1 = threading.Thread(clearall.clear_all(tokens.red))
-    t2 = threading.Thread(clearall.clear_all(tokens.green))
-    t3 = threading.Thread(clearall.clear_all(tokens.yellow))
-    t4 = threading.Thread(clearall.clear_all(tokens.white))
-    t5 = threading.Thread(clearall.clear_all(tokens.black))
+    t1 = threading.Thread(clear_all.clear_all(tokens.red))
+    t2 = threading.Thread(clear_all.clear_all(tokens.green))
+    t3 = threading.Thread(clear_all.clear_all(tokens.yellow))
+    t4 = threading.Thread(clear_all.clear_all(tokens.white))
+    t5 = threading.Thread(clear_all.clear_all(tokens.black))
 
     t1.start()
     t2.start()
--- methods/project.py
+++ methods/project.py
@@ -2,6 +2,12 @@ import requests
 import urls
 
 
+def get_all_projects(token):
+    url = "{}project?limit=100&offset=0".format(urls.qase)
+    headers = {"accept": "application/json", "token": token}
+    return requests.get(url, headers=headers)
+
+
 def remove_project_by_code(token, code):
     url = "{}project/{}".format(urls.qase, code)
     headers = {"accept": "application/json", "token": token}
