02a24097228f92c8cf4f69e24fc33e6a36a71d8c
burzumba <belyakov38@gmail.com>
2023-12-13 14:22:53 +0100

    удалил многопоточность

--- steps/clear_all.py
+++ steps/clear_all.py
@@ -1,6 +1,7 @@
 from steps import remove_all_attachments as attachments, remove_all_projects as projects
 
 
-def clear_all(token):
-    attachments.remove_all_attachments(token)
-    projects.remove_all_projects(token)
+def clear_all(tokens):
+    for token in tokens:
+        attachments.remove_all_attachments(token)
+        projects.remove_all_projects(token)
